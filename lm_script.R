aa9f761e011f0cb30c596228de69a2d7c00d6d6e
Kangping Yang <kangpiny@umich.edu>
2019-12-19 23:14:40 -0500

    Debug Filter_model function

--- lm_script.R
+++ lm_script.R
@@ -92,6 +92,7 @@ summary(selection)
 coef(selection,40)
 plot(selection,scale = "Cp")
 
+##All house in bejing model
 model_selct <- lm(price ~ square + bathRoom + buildingType + constructionTime + renovationCondition + elevator + subway + district + total_season, data = data)
 mat_full <- model.matrix(model_selct)
 ##mat_full
@@ -144,19 +145,47 @@ beta_data <- gather(beta_data,"class","price",2:4)
 ggplot(beta_data) + aes(x = X1, y = price , color = class, stroke = 2) + geom_line(size=1.5)
 
 
-##Data in ChaoYang
+##Function Conneced to Database inquiry data
 Filter_model <- function(inquaried_data){
   library(forecast)
   subdata <- inquaried_data[[1]]
-  subdata$season <- as.factor(subdata$season)
+  subdata$season <- as.factor(as.numeric(subdata$season))
+  if (inquaried_data[[4]] == F){
+    return(warning("Inference Unavailable due to too small subset of houses selected"))
+  }
   if (inquaried_data[[2]]==F && inquaried_data[[3]]==F){
     model_build <- lm(price ~ square + bathroom + constructiontime + renovationcondition + elevator + subway + season, data = subdata)
+    num_season <- length(table(subdata$season))
+    betas <- c(0,model_build$coefficients[10:(8+num_season)])
+    beta0 <- model_build$coefficients[1] + mean(subdata$bathroom)*model_build$coefficients[3]
+  }else if(inquaried_data[[2]]==T && inquaried_data[[3]]==F){
+    model_build <- lm(price ~ square + bathroom + constructiontime + renovationcondition + elevator + subway + district + season, data = subdata)
+    num_season <- length(table(subdata$season))
+    betas <- c(0,model_build$coefficients[21:(19+num_season)])
+    district_stat <- table(subdata$district)
+    weight_dist <- district_stat/sum(district_stat)
+    beta0 <- model_build$coefficients[1] + mean(subdata$bathroom)*model_build$coefficients[3] +sum(weight_dist*c(0,model_build$coefficients[10:20]))
+  }else if(inquaried_data[[2]]==F && inquaried_data[[3]]==T){
+    model_build <- lm(price ~ square + bathroom + buildingtype + constructiontime + renovationcondition + elevator + subway + season, data = subdata)
+    num_season <- length(table(subdata$season))
+    if (length(model_build$coefficients) - num_season + 1 == 11){
+      betas <- c(0,model_build$coefficients[12:(10+num_season)])
+    }else{
+      betas <- c(0,model_build$coefficients[11:(9+num_season)])
+    }
+    beta0 <- model_build$coefficients[1] + mean(subdata$bathroom)*model_build$coefficients[3]
+  }else{
+    model_build <- lm(price ~ square + bathroom + buildingtype + constructiontime + renovationcondition + elevator + subway + district + season, data = subdata)
+    num_season <- length(table(subdata$season))
+    if (length(model_build$coefficients) - num_season + 1 == 22){
+      betas <- c(0,model_build$coefficients[23:(21+num_season)])
+    }else{
+      betas <- c(0,model_build$coefficients[22:(20+num_season)])
+    }
+    district_stat <- table(subdata$district)
+    weight_dist <- district_stat/sum(district_stat)
+    beta0 <- model_build$coefficients[1] + mean(subdata$bathroom)*model_build$coefficients[3] +sum(weight_dist*c(0,model_build$coefficients[12:22]))
   }
-  num_season <- length(table(subdata$season))
-  model_build$coefficients
-  betas <- c(0,model_build$coefficients[10:(8+num_season)])
-
-  beta0 <- model_build$coefficients[1] + mean(subdata$bathroom)*model_build$coefficients[3]
   betas <- betas + beta0
   beta_lower <- betas
   beta_upper <- betas
@@ -171,19 +200,19 @@ Filter_model <- function(inquaried_data){
   ln_low_rate <- log(beta_low_rate)
   predict_low_rate <- forecast(arima(ln_low_rate,c(8,1,1)))
   ##higher raising speed
-  higher_beta <- c(beta_upper,beta_upper[length(beta_upper)]*exp(predict_ln_rate$upper[1,1]))
-  beta_high_rate <- higher_beta[2:length(higher_beta)]/higher_beta[-length(higher_beta)]
-  ln_high_rate <- log(beta_high_rate)
-  predict_high_rate <- forecast(arima(ln_high_rate,c(8,1,1)))
+  ##higher_beta <- c(beta_upper,beta_upper[length(beta_upper)]*exp(predict_ln_rate$upper[1,1]))
+  ##beta_high_rate <- higher_beta[2:length(higher_beta)]/higher_beta[-length(higher_beta)]
+  ##ln_high_rate <- log(beta_high_rate)
+  ##predict_high_rate <- forecast(arima(ln_high_rate,c(8,1,1)))
   for (i in 1:10){
     betas[length(betas)+1] <- exp(predict_ln_rate$mean[i])*betas[length(betas)]
     lower_beta[length(lower_beta)+1] <- exp(predict_low_rate$mean[i])*lower_beta[length(lower_beta)]
-    higher_beta[length(higher_beta)+1] <- exp(predict_high_rate$mean[i])*higher_beta[length(higher_beta)]
+    ##higher_beta[length(higher_beta)+1] <- exp(predict_high_rate$mean[i])*higher_beta[length(higher_beta)]
   }
   lower_beta <- lower_beta[-length(lower_beta)]
-  higher_beta <- higher_beta[-length(higher_beta)]
-  beta_data <- data.frame(matrix(c(1:(10+num_season),lower_beta,higher_beta,betas),(10+num_season),4))
-  beta_data <- gather(beta_data,"class","price",2:4)
+  ##higher_beta <- higher_beta[-length(higher_beta)]
+  beta_data <- data.frame(matrix(c(1:(10+num_season),lower_beta,betas),(10+num_season),3))
+  beta_data <- gather(beta_data,"class","price",2:3)
   ggplot(beta_data) + aes(x = X1, y = price , color = class, stroke = 2) + geom_line(size=1.5)
 }
 
@@ -242,7 +271,7 @@ plot(lower_beta)
 plot(higher_beta)
 beta_data <- data.frame(matrix(c(1:40,lower_beta,higher_beta,betas),40,4))
 beta_data <- gather(beta_data,"class","price",2:4)
-ggplot(beta_data) + aes(x = X1, y = price , color = class, stroke = 2) + geom_line(size=1.5)
+ggplot(beta_data) + aes(x = X1, y = price , color = class, stroke = 2) + geom_line(size=1)
 
 ##Data in Haidian
 data_Haidian <- data[which(data$district == "HaiDian"),]
