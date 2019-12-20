58b4219322ce1910d80f6de8e74b6933b64d5185
kangpingy <57377098+kangpingy@users.noreply.github.com>
2019-12-20 15:45:09 -0500

    Update CI for prediction plot

--- Prediction_Model_Function.R
+++ Prediction_Model_Function.R
@@ -48,30 +48,32 @@ Filter_model <- function(inquaried_data){
   coeff_info <- model_summary$coefficients[,c(-2,-3)]
   coeff_info <- coeff_info[1:(dim(coeff_info)[1]-num_season+1),]
   model_stat <- c(model_summary$r.squared , model_summary$adj.r.squared)
-
   beta_rate <- betas[2:length(betas)]/betas[-length(betas)] # proportion of adjacent seasons
   ln_rate <- log(beta_rate)
   predict_ln_rate <- forecast(arima(ln_rate,c(8,1,1)))
   predict_beta <- rep(0,10)
   ##lower raising speed
-  lower_beta <- c(beta_lower,beta_lower[length(beta_lower)]*exp(predict_ln_rate$lower[1,1]))
-  beta_low_rate <- lower_beta[2:length(lower_beta)]/lower_beta[-length(lower_beta)]
-  ln_low_rate <- log(beta_low_rate)
-  predict_low_rate <- forecast(arima(ln_low_rate,c(8,1,1)))
+  ##lower_beta <- c(beta_lower,beta_lower[length(beta_lower)]*exp(predict_ln_rate$lower[1,1]))
+  ##beta_low_rate <- lower_beta[2:length(lower_beta)]/lower_beta[-length(lower_beta)]
+  ##ln_low_rate <- log(beta_low_rate)
+  ##predict_low_rate <- forecast(arima(ln_low_rate,c(8,1,1)))
   ##higher raising speed
   ##higher_beta <- c(beta_upper,beta_upper[length(beta_upper)]*exp(predict_ln_rate$upper[1,1]))
   ##beta_high_rate <- higher_beta[2:length(higher_beta)]/higher_beta[-length(higher_beta)]
   ##ln_high_rate <- log(beta_high_rate)
   ##predict_high_rate <- forecast(arima(ln_high_rate,c(8,1,1)))
   for (i in 1:8){ # predict betas for the next 2 years(8 seasons)
+    beta_lower[length(beta_lower)+1] <- exp(predict_ln_rate$lower[i,1])*betas[length(betas)]
+    beta_upper[length(beta_upper)+1] <- exp(predict_ln_rate$upper[i,1])*betas[length(betas)]
     betas[length(betas)+1] <- exp(predict_ln_rate$mean[i])*betas[length(betas)] # betas grow at this point
-    lower_beta[length(lower_beta)+1] <- exp(predict_low_rate$mean[i])*lower_beta[length(lower_beta)]
+    ##lower_beta[length(lower_beta)+1] <- exp(predict_low_rate$mean[i])*lower_beta[length(lower_beta)]
     ##higher_beta[length(higher_beta)+1] <- exp(predict_high_rate$mean[i])*higher_beta[length(higher_beta)]
   }
-  lower_beta <- lower_beta[-length(lower_beta)]
+  ##lower_beta <- lower_beta[-length(lower_beta)]
   ##higher_beta <- higher_beta[-length(higher_beta)]
-  beta_data <- data.frame(matrix(c(plotxaxis,(plotxaxis[length(plotxaxis)]+1):(plotxaxis[length(plotxaxis)]+8),lower_beta,betas),(8+num_season),3))
-  beta_data <- gather(beta_data,"class","price",2:3)
+  beta_data <- data.frame(matrix(c(plotxaxis,(plotxaxis[length(plotxaxis)]+1):(plotxaxis[length(plotxaxis)]+8),beta_lower,beta_upper,betas),(8+num_season),4))
+  names(beta_data)[2:4] <- c(" Lower Bound"," Upper Bound","(Predicted) Mean")
+  beta_data <- gather(beta_data,"class","price",2:4)
   beta_data[,1] <- (beta_data[,1]-1)/4 + 2010
   names(beta_data)[1] <- "year"
   prediction_plot <-ggplot(beta_data) + aes(x = year, y = price , color = class, stroke = 2) + geom_line(size=1.5)
